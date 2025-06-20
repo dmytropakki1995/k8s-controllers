fdf3e7a2e076b1705a3989dcb06622ccb2e82349
Dmytro Pakki <dmytro.pakki@standardindustries.com>
2025-06-20 18:37:35 +0300

    add code

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
 */
 package main
 
-import "github.com/dimapakki95/k8s-controllers/cmd"
+import "github.com/yourusername/k8s-controller-tutorial/cmd"
 
 func main() {
 	cmd.Execute()
