b6039409cf407e1a3e4fb0cc0da0aac3f27c5baf
Dmytro Pakki <dimapakki95@gmail.com>
2025-08-27 00:01:11 +0300

    test-task: add fixed manifest & application code

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 import os
 import mimetypes
 import sqlite3
 import time
 
 app = Flask(__name__)
-UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "/uploads")
+UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "./uploads") # "/app/uploads") # the default path os '/app/uploads' instead of /'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-db_path = "/app/data/uploads.db"
+db_path = "./app/data/uploads.db"
 os.makedirs(os.path.dirname(db_path), exist_ok=True)
 
 # Initialize the SQLite database
@@ -27,6 +27,17 @@ def init_db():
 @app.route('/health')
 def health():
     return 'OK', 200
+
+@app.route('/uploads/<filename>')
+def get_file(filename):
+    file_path = os.path.join(UPLOAD_FOLDER, filename)
+
+    # Check if the file exists
+    if not os.path.isfile(file_path):
+        return {"Error": f"File {filename} does not exist in {UPLOAD_FOLDER}"}, 404
+    
+    # Send the file
+    return send_from_directory(UPLOAD_FOLDER, filename)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -47,8 +58,12 @@ def upload_file():
     mime_type = mimetypes.guess_type(file_path)[0] or 'unknown'
     print(f"MIME type: {mime_type}")
 
+    
+    # This method should not use init_db() every time when the file is uploaded. It fails if this method is called more than once.
+    # init_db() is better to use in a separate method or even a container like a migration container.
     # Store metadata in the database
     init_db()
+
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''INSERT INTO uploads (filename, upload_time, mime_type) VALUES (?, ?, ?)''',
